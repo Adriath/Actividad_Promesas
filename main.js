d0c365d1d2795fb5d9dec86597589b85146ed2e8
Adriath <adrianarjonabravo@gmail.com>
2024-02-14 21:45:58 +0100

    Ahora distingue entre la primera ejecución y las demás

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ https://api.openweathermap.org/data/2.5/weather?q=Galapagar&appid=86d830bb22abc9
 
 let appid = "86d830bb22abc9b1b3fe4fb21a030040" ;
 let urlWeather ="";
+let primeraVez = true ; // Esta variabe sirve para distinguir la primera vez de las demás en caso de que se pulse el botón con el campo en blanco.
 
 document.getElementById("botonTiempo").addEventListener("click", cargaCiudad) ;
 
@@ -14,11 +15,6 @@ function cargaCiudad() {
     let btnCiudad = document.getElementById("ciudad") ;
     ciudad = btnCiudad.value ;
 
-    if (ciudad.trim().length === 0) {
-        
-        ciudad = "Galapagar" ; 
-    }
-
     urlWeather =`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${appid}&units=metric&mode=xml`;
 
     let xhr = new XMLHttpRequest() ;
@@ -27,16 +23,33 @@ function cargaCiudad() {
         
         if (this.readyState == 4)
         {
-            // Si la respuesta es correcta
             if (this.status == 200)
+                // Si la respuesta es correcta
             { 
+                primeraVez = false ; // Deja de ser la primera vez
                 cargarXML(this) ;
             }
-            // Si la respuesta es incorrecta
             else
+                // Si la respuesta es incorrecta
             { 
-                document.getElementById("demo").innerHTML = "" ; // Limpia la tabla
-                document.getElementById("error").innerHTML = "Ciudad no encontrada" ; // Muestra el error
+                
+                if (primeraVez)
+                    // La primera vez mostrará un mensaje de error específico
+                {
+                    document.getElementById("error").innerHTML = "Debes introducir una ciudad" ; // Muestra el error
+                    
+                    if (ciudad.trim().length !== 0)
+                        // Si se introduce una ciudad incorrecta después de haber dado al botón la primera vez (con el cambpo en blanco)...
+                    {
+                        primeraVez = false ; // ... cambiará la variable primeraVez a false para que muestre el siguiente tipo de error
+                    }
+                }
+                else
+                    // La segunda vez y sucesivas mostrará el siguiente tipo de error
+                {
+                    document.getElementById("demo").innerHTML = "" ; // Limpia la tabla
+                    document.getElementById("error").innerHTML = "Ciudad no encontrada" ; // Muestra el error
+                }
             }
         }
     } ;
@@ -67,8 +80,5 @@ function cargarXML(xml) {
 
     document.getElementById("demo").innerHTML = table ; // Pinta la tabla
     document.getElementById("error").innerHTML = "" ; // Limpia el mensaje de error
-
-    
-
     
 }
